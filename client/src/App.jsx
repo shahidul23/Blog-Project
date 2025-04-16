f5da263494beb5a1581a1851dd0628209d48aa3a
Shahidul islam <Shahidul239.si@gmail.com>
2025-04-16 08:37:04 +0600

    create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Singin from './pages/Singin'
+import SignUp from './pages/SignUp'
+import Dashboard from './pages/Dashboard'
 
 export default function App() {
   return (
-    <div>
-      <h1 className='text-3xl text-red-500'>Hello Blogger</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/about' element={<About />}/>
+        <Route path='/contact' element={<Contact />}/>
+        <Route path='/sing-in' element={<Singin />}/>
+        <Route path='/sign-up' element={<SignUp />}/>
+        <Route path='/dashboard' element={<Dashboard />}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
---
+++ client/src/pages/Singin.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Singin() {
+  return (
+    <div>
+      <h1>Sign In</h1>
+    </div>
+  )
+}
+
+export default Singin
